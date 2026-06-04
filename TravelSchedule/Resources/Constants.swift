a37b7e43419863dd5e2b153c0df2ec4f9c04d5cb
Павел Кузнецов <pavelkuznetsov@MacBook-Pro-Pavel.local>
2026-06-05 00:52:32 +0300

    Добавлен экран настроек и пользовательского соглашения

--- TravelSchedule/Resources/Constants.swift
+++ TravelSchedule/Resources/Constants.swift
@@ -8,5 +8,5 @@
 enum Constants {
     
     static let apiKey = "28908aff-03f2-4c1a-95e5-156cd47f5bc8"
-    
+    static let agreementURLString = "https://yandex.ru/legal/timetable_termsofuse/ru/"
 }
